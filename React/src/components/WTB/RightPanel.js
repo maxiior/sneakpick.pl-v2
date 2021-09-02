dd056e313317a1f97ab1f4924a9e722fccfa259a
maxiior <59049370+maxiior@users.noreply.github.com>
2021-09-02 15:15:12 +0200

    little items panel fix

--- React/src/components/WTB/RightPanel.js
+++ React/src/components/WTB/RightPanel.js
@@ -6,7 +6,7 @@ import { connect } from "react-redux";
 const StyledRightPanel = styled.div`
   display: block;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   box-sizing: border-box;
   padding-left: 250px;
   background-color: ${({ theme }) => theme.white};
